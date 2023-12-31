c33e2bb9eaa90d3c1308e8fda08ee2cd208bdc01
tradervicky <vickygupta031@gmail.com>
2023-12-31 09:26:31 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import './App.css';
+import Finance from './Nav Pages/Finance';
+import Footer from './Nav Pages/Footer';
+import Health from './Nav Pages/Health';
+import Sports from './Nav Pages/Sports';
+import Upsc from './Nav Pages/Upsc';
+import Navbar from './Navbar';
+import News from './News';
+import Search from './Nav Pages/Search';
+import Technology from './Nav Pages/Technology';
+import Global from './Nav Pages/Global';
 
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      
+      <BrowserRouter>
+      <Navbar/>
+      <Routes>
+      <Route path='/' element={<News/>}/>
+      <Route path='/upsc' element={<Upsc/>}/>
+      <Route path='/finance' element={<Finance/>}/>
+      <Route path='/sports' element={<Sports/>}/>
+      <Route path='/health' element={<Health/>}/>
+      <Route path='/search' element={<Search/>}/>
+      <Route path='/technology' element={<Technology/>}/>
+      <Route path='/global' element={<Global/>}/>
+      </Routes>
+      </BrowserRouter>
+      <Footer/>
+      
     </div>
   );
 }
